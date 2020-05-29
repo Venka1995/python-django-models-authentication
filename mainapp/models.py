f19feedbaec60cd09e2f759f932ed9321f22f452
Venka1995 <Venkateshaks598@gmail.com>
2020-05-29 18:16:17 +0530

    final commit

--- mainapp/models.py
+++ mainapp/models.py
@@ -8,6 +8,7 @@ class BlogPost(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="posts", on_delete=models.CASCADE)
     body = models.TextField()
     postdate = models.DateTimeField(auto_now_add=True, blank=True)
+    tags = models.ManyToManyField('Tag', related_name='posts')
 
     def __str__(self):
         return self.title
@@ -18,7 +19,14 @@ class BlogPost(models.Model):
 
 class Tag(models.Model):
     name = models.CharField(max_length=50, unique=True)
-    tags = models.ManyToManyField('Tag', related_name='posts')
+
+
+
+    def clean(self):
+        self.name=self.name.lower()
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('post', args=[str(self.name)])
